b1b811e460b58b6100792c28126b3aa2a699b82f
Diego Gutiérrez <diego804@gmail.com>
2023-03-17 07:58:30 -0500

    Create basic routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import { Dashboard } from "./components/Dashboard"
+import { Login } from './components/Login';
+import { Register } from './components/Register';
 
 function App() {
   return (
-    <>
-    <div>hello world</div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>    
+    <div className="bg-slate-300 h-screen text-white flex">
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/Register" element={<Register/>} />
+        <Route path="/Login" element={<Login/>} />
+      </Routes> 
+    </div>
   );
 }
 
