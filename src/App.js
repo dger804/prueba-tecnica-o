9346199da44a02fc5577232b6e7dfc166af15934
Diego Gutiérrez <diego804@gmail.com>
2023-03-17 08:29:49 -0500

    Add authContext

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 import { Dashboard } from "./components/Dashboard"
 import { Login } from './components/Login';
 import { Register } from './components/Register';
+import { AuthProvider } from './context/authContext';
 
 function App() {
   return (
     <div className="bg-slate-300 h-screen text-white flex">
-      <Routes>
-        <Route path="/" element={<Dashboard/>} />
-        <Route path="/Register" element={<Register/>} />
-        <Route path="/Login" element={<Login/>} />
-      </Routes> 
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/Register" element={<Register/>} />
+          <Route path="/Login" element={<Login/>} />
+        </Routes> 
+      </AuthProvider>
     </div>
   );
 }
