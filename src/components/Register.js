0525cd9e5c986b5a9e8710c295b915e6c4c9508f
Diego Gutiérrez <diego804@gmail.com>
2023-03-17 09:14:02 -0500

    Get data for register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,65 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
 export const Register = () =>{
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    birthday: ""
+  })  
+
+  const handleChange = ({target: {name, value}}) => setUser({...user, [name]: value})  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user)
+
+  }
+
   return(
     <>
     <div>
       <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>Email</div>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div>Password</div>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div>Re-type Password</div>
+          <input
+            type="password"
+            name="repassword"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div>Date of Birthday</div>
+          <input
+            type="date"
+            name="birthday"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">
+          Register
+        </button>
+        <div>
+          Already have an account? <Link to="/">Login here</Link>
+        </div>
+      </form>
     </div>
     </>
   )
