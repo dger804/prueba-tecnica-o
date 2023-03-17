ae8834d0d36ff5a6e7e032eaf3727fc1a3e049e4
Diego Gutiérrez <diego804@gmail.com>
2023-03-17 08:30:04 -0500

    Add Login frorm

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
 export const Login = () =>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return(
     <>
     <div>
       <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>Email</div>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <div>Password</div>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">
+          Login
+        </button>
+        <div>
+          Don't have an account? Sign Up <Link to="/register">Here</Link>
+        </div>
+      </form>
     </div>
     </>
   )
